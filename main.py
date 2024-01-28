6a713c35bca086c1ab6611572366182f03749703
RUBY <teget@mail.ru>
2024-01-28 17:37:00 +0300

    mistral added

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import requests.exceptions
 from gpt.gpt_funcs import request_completion, create_request_template
 from preparation.load_dataset import load_dataset
-from enviromet.basics import parse
+from enviroment.basics import parse
 from estimate.metrics import calculate_classification_metrics
 from preparation.extracting import string_to_continuous
-from enviromet.basics import create_result_directory
+from enviroment.basics import create_result_directory
 from sys import argv
 from dotenv import load_dotenv
 from samplers import Sampler
 from samplers.occurrence_sampler import OccurrenceSampler
 from samplers.strict_sampler import StrictSampler
+from mistral.mistral_funcs import create_request_template_mistral
 
 
 load_dotenv()
@@ -36,42 +37,75 @@ for smp in Sampler.__subclasses__():
         sampler = smp()
         sampler.configure(config)
 
-for request in range(len(sample_features_test)):
+for request in range(10): #len(sample_features_test)
 
     sampler.configure({"class": sample_targets_test[request].split(", ")[0]})
     samples = sampler.sample(list(zip(sample_features_train, sample_targets_train)),
                              list(zip(sample_features_test, sample_targets_test)))
-    template = create_request_template(samples,
-                                       seed=config["seed"],
-                                       name=f"{config['subject']}_doctor")
+    if config["nnet"] == "gpt":
+        template = create_request_template(samples,
+                                           seed=config["seed"],
+                                           name=f"{config['subject']}_doctor")
+        try:
+            completion_response = request_completion(question=sample_features_test[request],
+                                                     name=f"{config['subject']}_doctor",
+                                                     request=template,
+                                                     engine=config["engine"],
+                                                     )
+            # print(completion_response.choices[0].message.content)
 
-    try:
-        completion_response = request_completion(question=sample_features_test[request],
-                                                 name=f"{config['subject']}_doctor",
-                                                 request=template,
-                                                 engine=config["engine"],
-                                                 )
+        except requests.exceptions.ConnectionError as e:
+            log.append(e)
+            with open(path / "log.txt", "w") as file:
+                file.write("".join(log))
+            raise
+
+        except Exception as e:
+            # raise
+            log.append(f"Request_{request} missed\n"
+                       f"Content: {sample_features_test[request]}\n"
+                       f"Reason: {e}\n\n")
+            missed_requests.append(request)
+            continue
+
+        else:
+            result.append(completion_response.choices[0].message.content)
+            history.append(f"request: {sample_features_test[request]}\n"
+                           f"response: {completion_response.choices[0].message.content}\n"
+                           f"true: {sample_targets_test[request]}")
+
+    elif config["nnet"] == "mistral":
+        template = create_request_template_mistral(samples,
+                                                   seed=config["seed"])
+        try:
+            completion_response = request_completion(question=sample_features_test[request],
+                                                     name=f"{config['subject']}_doctor",
+                                                     request=template,
+                                                     engine=config["engine"],
+                                                     )
+            print(completion_response.choices[0].message.content)
+
+        except requests.exceptions.ConnectionError as e:
+            log.append(e)
+            with open(path / "log.txt", "w") as file:
+                file.write("".join(log))
+            raise
+
+        except Exception as e:
+            # raise
+            log.append(f"Request_{request} missed\n"
+                       f"Content: {sample_features_test[request]}\n"
+                       f"Reason: {e}\n\n")
+            missed_requests.append(request)
+            continue
+
+        else:
+            result.append(completion_response.choices[0].message.content)
+            history.append(f"request: {sample_features_test[request]}\n"
+                           f"response: {completion_response.choices[0].message.content}\n"
+                           f"true: {sample_targets_test[request]}")
         print(completion_response.choices[0].message.content)
 
-    except requests.exceptions.ConnectionError as e:
-        log.append(e)
-        with open(path / "log.txt", "w") as file:
-            file.write("".join(log))
-        raise
-
-    except Exception as e:
-        # raise
-        log.append(f"Request_{request} missed\n"
-                   f"Content: {sample_features_test[request]}\n"
-                   f"Reason: {e}\n\n")
-        missed_requests.append(request)
-        continue
-
-    else:
-        result.append(completion_response.choices[0].message.content)
-        history.append(f"request: {sample_features_test[request]}\n"
-                       f"response: {completion_response.choices[0].message.content}\n"
-                       f"true: {sample_targets_test[request]}")
 
 if missed_requests:
     count = 0
@@ -87,15 +121,15 @@ full_data = {
     "valid": sample_targets_train + sample_targets_test
 }
 
-for title in full_data.keys():
-    calculate_classification_metrics(y_true=string_to_continuous(income_data=sample_targets_test,
-                                                                 classes=config["classes"]),
-                                     y_pred=string_to_continuous(income_data=result,
-                                                                 classes=config["classes"]),
-                                     config=config,
-                                     path=path,
-                                     title=title
-                                     )
-
+# for title in full_data.keys():
+#     calculate_classification_metrics(y_true=string_to_continuous(income_data=sample_targets_test,
+#                                                                  classes=config["classes"]),
+#                                      y_pred=string_to_continuous(income_data=result,
+#                                                                  classes=config["classes"]),
+#                                      config=config,
+#                                      path=path,
+#                                      title=title
+#                                      )
+#
 with open(path / "history.txt", "w") as file:
     file.write("".join(history))
